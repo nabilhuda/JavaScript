9bc9b926fe9cb2a7686181c82e14b8ef8fdaab32
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-05-29 17:55:33 +0600

    Add files via upload

---
+++ The number of Positive and Negative in ten numbers.js
@@ -0,0 +1,20 @@
+
+
+// The number of Positive and Negative in ten numbers
+
+
+var i = 1;
+var posicount = 0;
+var negacount = 0;
+var x;
+while( i<=10 ) {
+  x = prompt("Enter Your Numbers Here");
+  if( x>=0 ) {
+    posicount++;
+  } else {
+  	negacount++;
+  }
+  i++;
+}
+console.log("The number of Positive in ten numbers is " + posicount);
+console.log("The number of Negative in ten numbers is " + negacount);
