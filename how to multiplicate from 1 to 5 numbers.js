f30de7c96427f149a74d8d39c4e467b1e6e8d340
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-05-29 23:24:49 +0600

    Add files via upload

---
+++ how to multiplicate from 1 to 5 numbers.js
@@ -0,0 +1,13 @@
+
+
+// how to multiplicate(*) from 1 to 5 numbers
+
+
+var sum = 1;
+var i = 1;
+var n = 5;
+while( i<= n ) {
+  sum*=i;
+  i++;
+}
+console.log(sum + " the multiplication from 1 to 5");
