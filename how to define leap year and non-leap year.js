a94f140919ea9af54b9823d143843325ee0305e6
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-05-29 23:44:46 +0600

    Add files via upload

---
+++ how to define leap year and non-leap year.js
@@ -0,0 +1,14 @@
+
+
+// how to define leap year and non-leap year
+
+
+
+var year = 2016;
+if( year%400 === 0 ) {
+  console.log('leap Year');
+} else if( year%100 !== 0 && year%4 === 0 ) {
+  console.log(year + " Leap Year");
+} else {
+  console.log(year + " Not Leap Year");
+}
