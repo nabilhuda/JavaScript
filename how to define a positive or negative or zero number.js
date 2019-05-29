62b0b413add56b98301b260a9bfd164d3c7ec1dc
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-05-29 23:56:53 +0600

    Add files via upload

---
+++ how to define a positive or negative or zero number.js
@@ -0,0 +1,13 @@
+
+
+// how to define a positive or negative or zero number
+
+
+var a = -2;
+if( a===0 ) {
+  console.log(a + " this number is equal with 0");
+} else if( a>0 ) {
+  console.log(a + " bigger than 0");
+} else {
+  console.log(a + " smaller than 0");
+}
