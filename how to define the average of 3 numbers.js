8cede9a6b8c20814236cd0d881bf82904e25f1b5
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-05-29 23:34:32 +0600

    Add files via upload

---
+++ how to define the average of 3 numbers.js
@@ -0,0 +1,11 @@
+
+
+// how to define the average of 3 numbers
+
+
+var a = 10;
+var b= 11;
+var c = 12;
+var sum = a+b+c;
+var average = sum/3;
+console.log(average + " the average of " + a + ", " + b + " and " + c + " numbers");
