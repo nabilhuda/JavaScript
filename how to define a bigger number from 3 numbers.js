5d74ca4d83988bb5a4a34780f07030c3f23ed0a4
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-05-30 15:30:43 +0600

    Add files via upload

---
+++ how to define a bigger number from 3 numbers.js
@@ -0,0 +1,15 @@
+
+
+// how to define a bigger number from 3 numbers
+
+
+var a = 13;
+var b = 14;
+var c = 15;
+if( a>b && a>c ) {
+  console.log(a + " the number is bigger than " + b + " and " + c);
+} else if ( b>a && b>c ) {
+  console.log(b + " the number is bigger than " + a + " and " + c);
+} else {
+  console.log(c + " the number is bigger than " + a + " and " + b);
+}
