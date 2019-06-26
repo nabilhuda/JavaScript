fabb4385b4b62913a53cd3dfee6480e6e00f0687
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-06-26 23:46:56 +0600

    Add files via upload

---
+++ how to add some numbers.js
@@ -0,0 +1,15 @@
+
+
+// how to add some numbers
+
+
+var i = 1;
+var sum = 0;
+var n = 5;
+var x;
+while( i<n ) {
+  x = prompt('Please Enter Your Numbers');
+  sum+=Number(x);
+  i++;
+}
+console.log("The result of some numbers addition is " + sum);
