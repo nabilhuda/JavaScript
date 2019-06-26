4460df913a6bff71a3c09672452ead0081de7b9e
nabilhuda <45496658+nabilhuda@users.noreply.github.com>
2019-06-26 23:36:24 +0600

    Add files via upload

---
+++ how to convert lowercase to uppercase and  uppercase to lowercase.js
@@ -0,0 +1,20 @@
+
+
+// how to convert lowercase to uppercase and  uppercase to lowercase
+
+
+// how to covert lowercase to uppercase
+var text1 = 'n';
+var text2 = text1.toUpperCase();
+console.log(text2);
+
+// how to convert uppercase to lowercase
+var text3 = 'N';
+var text4 = text1.toLowerCase();
+console.log(text4);
+
+// how to uppercase the first letter of sentence
+var sentence = 'this example assumes at least one slide has been filled out and saved.';
+console.log(
+  sentence[0].toUpperCase() + sentence.slice(1)
+);
