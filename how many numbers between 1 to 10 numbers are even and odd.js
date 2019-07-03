f7d70a70bb9436be823cee23ced0600753ef8fc4
Nabil Huda <45496658+nabilhuda@users.noreply.github.com>
2019-07-03 22:57:45 +0600

    Add files via upload

---
+++ how many numbers between 1 to 10 numbers are even and odd.js
@@ -0,0 +1,18 @@
+
+
+// how many numbers in the 10 numbers are even and odd
+
+
+var i = 1;
+var evencount = 0;
+var oddcount = 0;
+while( i<=10 ){
+  if( i%2===0 ) {
+    evencount++;
+  } else {
+    oddcount++;
+  }
+  i++;
+}
+console.log('The Even number between 1 to 10 is ' + evencount);
+console.log('The Odd number between 1 to 10 is ' + oddcount);
