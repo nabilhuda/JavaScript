6c25ee8f5bb82f5182024b3cbee04d44da3f7478
Nabil Huda <45496658+nabilhuda@users.noreply.github.com>
2020-05-02 19:21:37 +0600

    Add files via upload

---
+++ create-object.js
@@ -0,0 +1,10 @@
+var something = new Object();
+
+
+something.prothom = 'This is our first line';
+something.ditiyo = 'This is our second line';
+something.jakhusi = function() {
+	return this.prothom + ' ' + this.ditiyo;
+}
+
+alert(something.jakhusi());
